814df58699e7b9bf0019a28a29d7f41ed3a87c98
RicardoPogianeli <ricardopogianeli91@gmail.com>
2019-06-02 16:51:17 -0300

    Exercício PUCPR Números Primos e função " Ajuste de erro."

---
+++ Funcao_paramentros.py
@@ -0,0 +1,19 @@
+lista_num =[]
+for num in range(1,5):
+   lista_num.append(int(input("Digite um número")))
+print (lista_num)
+
+def numero_negativo(lista_num):
+   negativo = []
+   for item in lista:
+      if lista_num < 0:
+         negativo.append(item)
+   return negativo
+
+
+
+
+
+
+
+
--- Lista_numeros_primos.py
+++ Lista_numeros_primos.py
@@ -2,11 +2,13 @@ lista_num = []
 num_digitado = None
 
 while num_digitado != 0:
+   try:
+      num_digitado = int(input("Digite um Número:"))
 
-   num_digitado = int(input("Digite um Número:"))
-
-   if num_digitado > 0 :
-      lista_num.append(num_digitado)
+      if num_digitado > 0 :
+         lista_num.append(num_digitado)
+   except Exception as e:
+      print("caracter digitado e invalido")
 print(lista_num)
 
 '''
@@ -18,8 +20,11 @@ def determinar_primo(lista):
    for item in lista:
       if item%2==1:
          primos.append(item)
+
    return primos
 
+
+print (determinar_primo(lista_num))
 
 
 
